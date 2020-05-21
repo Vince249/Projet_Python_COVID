30cabfd4a96cbeac4a6b076ee189ef73fdc1c797
Vince249 <vincent.poupet@neuf.fr>
2020-05-21 17:31:11 +0200

    push environnement virtuel MAC

---
+++ virtual_environment_MAC/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/vince/Documents /One drive/OneDrive - De Vinci/Année 3/ESILV/Cours/Langage Python/Projet/Repo_Git_Projet_Coronavirus/virtual_environment_MAC/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
